12291fd002f91fa3bf35311803b95642885df86a
JulienChoukroun <53264970+JulienChoukroun@users.noreply.github.com>
2020-06-06 17:52:21 +0200

    Add files via upload

---
+++ Code/Test.java
@@ -0,0 +1,15 @@
+import utilisateur.Utilisateur;
+
+/**
+ * Lance la classe Utilisateur.
+ *
+ * @author julien choukroun
+ * @author jessica gourdon
+ *
+ */
+public class Test {
+    public static void main (String [] args){
+        Utilisateur user = new Utilisateur();
+    }
+
+}
